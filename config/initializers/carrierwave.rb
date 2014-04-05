dd2784220ea5bde109efc212863275b7d7bcd059
Alexander Vasyuk <alexandervasyuk@gmail.com>
2014-04-05 10:05:28 -0700

    Label for progress bar. Seprate storage to file in dev and testing and AWS in production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,4 +8,12 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'berkeleygag'                           # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+
+	if Rails.env.test? or Rails.env.cucumber? or Rails.env.development?
+		config.storage = :file
+		config.enable_processing = false
+	else
+		config.storage = :fog
+		config.enable_processing = true
+	end
 end
