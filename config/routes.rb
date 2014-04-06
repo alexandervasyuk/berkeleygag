f870ab575ef2ce6d3778050ee0334f4dc4f54b9d
Alexander Vasyuk <alexandervasyuk@gmail.com>
2014-04-05 17:22:09 -0700

    Footer

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Berkeleygag::Application.routes.draw do
   get "confirm/:token", to:"users#confirm"
   get "access_denied", to:"home#access_denied"
 
+  get "about", to:"static_pages#about"
+  get "contact", to:"static_pages#contact"
+
 
   match "*gibberish", to:"home#not_found"
 end
