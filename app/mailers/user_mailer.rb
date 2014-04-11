24569f315e77f2a65686afed1a8114438cfc627c
Alexander Vasyuk <alexandervasyuk@gmail.com>
2014-04-10 22:18:13 -0700

    Removed default address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,4 @@
 class UserMailer < ActionMailer::Base
-	default from: "registration@berkeleygag.com"
-
 	def confirm(user)
 		@user = user
 		mail( to: @user.email, subject: "BerkeleyGag registration" )
